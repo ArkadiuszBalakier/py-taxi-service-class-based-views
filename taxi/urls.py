ae07725123de93dd989ef98e9005fe15f6e89962
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2025-12-16 15:19:52 +0100

    implementing views , urls, and manufacturer_list.html

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import index
+from .views import index, ManufacturerView, CarListView, DriverListView, CarDetailView, DriverDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manufacturer/", ManufacturerView.as_view(), name="manufacturer-list"),
+    path("cars/", CarListView.as_view(), name="cars"),
+    path("drivers/", DriverListView.as_view(), name="drivers"),
+    path("car/<int:pk>/", CarDetailView.as_view(), name="car"),
+    path("driver/<int:pk>/", DriverDetailView.as_view(), name="driver"),
 ]
 
 app_name = "taxi"
