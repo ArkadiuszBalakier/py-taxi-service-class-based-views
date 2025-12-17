269082bc2b77ba96eb82710f28f5fefc5494bfed
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2025-12-17 09:27:05 +0100

    implementing car-details template

--- taxi/urls.py
+++ taxi/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path("manufacturer/", ManufacturerView.as_view(), name="manufacturer-list"),
     path("car/", CarListView.as_view(), name="car-list"),
     path("driver/", DriverListView.as_view(), name="driver-list"),
-    path("car/<int:pk>/", CarDetailView.as_view(), name="car"),
-    path("driver/<int:pk>/", DriverDetailView.as_view(), name="driver"),
+    path("car/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("driver/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
 ]
 
 app_name = "taxi"
