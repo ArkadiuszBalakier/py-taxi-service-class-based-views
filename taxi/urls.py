0486e14c7c83ed357dafb5c59360899a68583160
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2025-12-17 08:12:05 +0100

    implementing car_list template

--- taxi/urls.py
+++ taxi/urls.py
@@ -5,8 +5,8 @@ from .views import index, ManufacturerView, CarListView, DriverListView, CarDeta
 urlpatterns = [
     path("", index, name="index"),
     path("manufacturer/", ManufacturerView.as_view(), name="manufacturer-list"),
-    path("cars/", CarListView.as_view(), name="cars"),
-    path("drivers/", DriverListView.as_view(), name="drivers"),
+    path("car/", CarListView.as_view(), name="car-list"),
+    path("driver/", DriverListView.as_view(), name="driver-list"),
     path("car/<int:pk>/", CarDetailView.as_view(), name="car"),
     path("driver/<int:pk>/", DriverDetailView.as_view(), name="driver"),
 ]
