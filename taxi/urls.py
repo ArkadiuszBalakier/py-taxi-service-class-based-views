9752e948d4845a9bf1f66b110eff4c55af5cf508
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2025-12-17 11:00:16 +0100

    fixing: no prular names of urls car->cars etc, adding pagination block

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,9 +4,9 @@ from .views import index, ManufacturerView, CarListView, DriverListView, CarDeta
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturer/", ManufacturerView.as_view(), name="manufacturer-list"),
-    path("car/", CarListView.as_view(), name="car-list"),
-    path("driver/", DriverListView.as_view(), name="driver-list"),
+    path("manufacturers/", ManufacturerView.as_view(), name="manufacturer-list"),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("car/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("driver/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
 ]
